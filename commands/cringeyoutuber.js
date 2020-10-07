27b28b9cb3ae751a3d4cab2cc25a5f9a441f1b7f
Slenky <slenky@protonmail.com>
2020-10-07 16:13:21 +0200

    Delete cringeyoutuber.js

--- commands/cringeyoutuber.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'cringeyoutuber',
-	description: 'Posle youtube kanal',
-	async execute(message, args) {
-        message.channel.send('https://www.youtube.com/c/TommyInnit/');
-    },
-};
