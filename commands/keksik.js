2325e60229fab26785937d704c92db0f32ea9eae
Slenky <slenky@protonmail.com>
2020-10-08 15:04:28 +0200

    Update keksik.js

--- commands/keksik.js
+++ commands/keksik.js
@@ -1,7 +1,19 @@
+const Discord = require('discord.js')
+
 module.exports = {
 	name: 'keksik',
 	description: 'Posle keksik',
+	cooldown: 4,
 	async execute(message, args) {
-        message.channel.send(":cookie:");
+        const fetch = require("node-fetch");
+      	var json = await (await fetch("https://api.hyrousek.tk/useless/reddit?reddit=cookies")).json();
+      	if(!json.url) return;
+      
+      	var embed = new Discord.MessageEmbed()
+        	.setImage(json.url)
+        	.setTitle(json.title)
+        	.setFooter('API vytvoril 𝙃𝙮𝙧𝙤#8938 | Bota vytvoril Slenky#0001')
+        	.setURL('http://memesare.fun')
+     	message.channel.send(embed)
     },
 };
